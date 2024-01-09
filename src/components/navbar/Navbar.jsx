df0d7c38931bea434994ea699e3e2f471ff3be40
FrancoAlberti1997 <albertifranco1997@hotmail.com>
2024-01-09 10:11:45 -0300

    responsive nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../navbar/1mPossible.png"
 
 const Navbar = () => {
+
+    const [clicked, setClicked] = useState(false);
+
+    const toggleMenu = () => {
+    setClicked(!clicked);
+    };
+
+
   return (
     <nav>
         <div className="first-line">
@@ -11,7 +19,12 @@ const Navbar = () => {
             </div>
             <div className="container-nav info">
                 <div className="info-contacto">
-                    <div className="phone">
+
+                    <div id="mobile" onClick={toggleMenu}>
+                        {clicked ? <i className='fa fas fa-times'></i> : <i className='fa fas fa-bars'></i>}
+                    </div>
+
+                    <div className="phone phone-active">
                         <div className="icon-phone">
                             <i class="fa fa-solid fa-phone"></i>
                         </div>
@@ -20,6 +33,7 @@ const Navbar = () => {
                             <p className='number-info'>+541176385223</p>
                         </div>
                     </div>
+
                     <div className="select_lg">
                         <div className="text-languaje">ENG</div>
                         <div className="icon-language"><i class="fa fa-solid fa-chevron-down"></i></div>
@@ -27,13 +41,15 @@ const Navbar = () => {
                 </div>
             </div>
         </div>
-        <div className="second-line">
+        <div className="second-line second-line-active">
             <div className="nav-section"><p><a href="">Inicio</a></p></div>
             <div className="nav-section"><p><a href="">Investigación</a></p></div>
             <div className="nav-section"><p><a href="">Colaboradores</a></p></div>
             <div className="nav-section"><p><a href="">Mision</a></p></div>
             <div className="nav-section"><p><a href="">Contacto</a></p></div>
         </div>
+
+        
     </nav>
   )
 }
